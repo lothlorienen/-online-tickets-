8e5729a3803cf09e4a6e0ec4c6f73aedc0f2ffee
Eugene <eugene.kotsarev@gmail.com>
2020-11-22 20:40:22 +0300

    add home base

--- app/scripts/vendors.js
+++ app/scripts/vendors.js
@@ -3,24 +3,24 @@ import 'react-app-polyfill/ie11';
 import 'intersection-observer';
 import './polyfills';
 import 'html5shiv';
-import 'picturefill';
-import lazySizes from 'lazysizes';
-import 'lazysizes/plugins/native-loading/ls.native-loading';
-import 'lazysizes/plugins/object-fit/ls.object-fit';
+// import 'picturefill';
+// import lazySizes from 'lazysizes';
+// import 'lazysizes/plugins/native-loading/ls.native-loading';
+// import 'lazysizes/plugins/object-fit/ls.object-fit';
 import svgPolyfill from 'svg4everybody';
 // import jquery from 'jquery';
-import swiper from 'swiper';
+// import swiper from 'swiper';
 
 // window.$ = window.jQuery = jquery;
 window.svg4everybody = svgPolyfill;
-window.Swiper = swiper;
+// window.Swiper = swiper;
 
-lazySizes.cfg.lazyClass = 'lazy';
-lazySizes.cfg.srcAttr = 'data-original';
-lazySizes.cfg.loadMode = 1;
-lazySizes.cfg.nativeLoading = {
-  setLoadingAttribute: true,
-  disableListeners: {
-    scroll: true,
-  },
-};
+// lazySizes.cfg.lazyClass = 'lazy';
+// lazySizes.cfg.srcAttr = 'data-original';
+// lazySizes.cfg.loadMode = 1;
+// lazySizes.cfg.nativeLoading = {
+//   setLoadingAttribute: true,
+//   disableListeners: {
+//     scroll: true,
+//   },
+// };
